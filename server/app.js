9c6ed9d8b60a04349d870b0e673f4b8eefb77308
kalim-Asim <asim1306iiit@gmail.com>
2025-06-20 15:38:33 +0530

    deploy

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 app.use(cors({
   origin: "https://help-desk-iiita.vercel.app",
-  origin: "http://localhost:5173",
+  // origin: "http://localhost:5173",
 }));
 
 app.use(express.json());
